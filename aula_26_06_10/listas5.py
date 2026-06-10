8d83b704a3bf0607924700a9902d8c937d0ac886
Belizário <murilo.belizario12@gmail.com>
2026-06-10 10:57:57 -0300

    Adding lesson 26_06_10

---
+++ aula_26_06_10/listas5.py
@@ -0,0 +1,11 @@
+cadastro = [
+    ['Davi', [9.1, 9.2, 9.3]],
+    ['Gaspar', [5.1, 5.2, 5.3]],
+    ['Isabelly', [9.8, 9.9, 10]]
+]
+
+gaspar = cadastro[1][0]
+print(gaspar)
+
+nota_isa = cadastro[2][1][2]
+print(nota_isa)
