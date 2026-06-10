f87f039ac6b88875527945bc5b37d834dcfae138
Belizário <murilo.belizario12@gmail.com>
2026-06-10 11:07:39 -0300

    Modifying listas5.py

--- aula_26_06_10/listas5.py
+++ aula_26_06_10/listas5.py
@@ -4,8 +4,47 @@ cadastro = [
     ['Isabelly', [9.8, 9.9, 10]]
 ]
 
-gaspar = cadastro[1][0]
-print(gaspar)
+banco = []
 
-nota_isa = cadastro[2][1][2]
-print(nota_isa)
+# Adicionando o DAVI
+notas = []
+notas.append(9.1)
+notas.append(9.2)
+notas.append(9.3)
+
+aluno = []
+aluno.append('Davi')
+aluno.append(notas)
+
+banco.append(aluno)
+
+# Adicionando o GASPAR
+notas = []
+notas.append(5.1)
+notas.append(5.2)
+notas.append(5.3)
+
+aluno = []
+aluno.append('Gaspar')
+aluno.append(notas)
+
+banco.append(aluno)
+
+# Adicionando a ISABELLY
+notas = []
+notas.append(9.8)
+notas.append(9.9)
+notas.append(10)
+
+aluno = []
+aluno.append("Isabelly")
+aluno.append(notas)
+
+banco.append(aluno)
+
+print(cadastro)
+print(banco)
+
+#print(cadastro[1][0]) # Gaspar
+
+#print(cadastro[2][1][2]) # 10
